05e90701483ccda7f08720629775d99777cd5477
Rutvi Bheda <68680524+rutvibheda@users.noreply.github.com>
2025-07-12 15:36:50 -0700

    Time: 3 ms (93.16%) | Memory: 21.6 MB (29.18%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -4,9 +4,10 @@ class Solution:
         output = [intervals[0]]
 
         for start, end in intervals[1:]:
-            lastElement = output[-1][1]
-            if lastElement >= start:
-                output[-1][1] = max(lastElement, end)
+            lastElementToCompare = output[-1][1]   #acc first example it will be 3 
+            if start <= lastElementToCompare:
+                output[-1][1] = max(end, lastElementToCompare)
             else:
                 output.append([start,end])
+
         return output
