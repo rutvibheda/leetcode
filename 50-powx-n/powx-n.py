f31b7ff7ae646caf75d535b8b5201d7e76798154
Rutvi Bheda <68680524+rutvibheda@users.noreply.github.com>
2025-07-12 10:16:17 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (49.34%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,5 +1,6 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
+
         def helper(x,n):
             if x == 0:
                 return 0
@@ -13,8 +14,9 @@ class Solution:
             else:
                 return res
 
-        result = helper(x,abs(n))
-        if n >= 0:
+        result = helper(x, abs(n))
+        if n > 0:
             return result
         else:
             return (1/result)
+        
