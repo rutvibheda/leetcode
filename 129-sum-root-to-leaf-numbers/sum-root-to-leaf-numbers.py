2ee51502ec8a565950d0492b0e5625c0933c60c9
Rutvi Bheda <68680524+rutvibheda@users.noreply.github.com>
2025-07-14 18:07:29 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (36.71%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
@@ -9,13 +9,12 @@ class Solution:
         def dfs(node, num):
             if not node:
                 return 0
-
+            
             num = num * 10 + node.val
 
             if not node.left and not node.right:
                 return num
-            
-            return dfs(node.left, num) + dfs(node.right, num)
 
-        return dfs(root,0)
-                   
+            return dfs(node.left, num) +  dfs(node.right, num)
+
+        return dfs(root, 0)      
