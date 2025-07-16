6469ab012b84da2fded8dba6a70323d6672e2ba3
Rutvi Bheda <68680524+rutvibheda@users.noreply.github.com>
2025-07-15 22:17:21 -0700

    Time: 3 ms (80.33%) | Memory: 19 MB (9.69%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,9 +4,10 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         if len(nums) <= 1:
-            return
+            return 
         count = 0
         for i in range(len(nums)):
             if nums[i] != 0:
                 nums[i], nums[count] = nums[count], nums[i]
-                count += 1        
+                count += 1
+        
